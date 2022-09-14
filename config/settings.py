16fe276022defce6cda214c1c2ff4c708f770999
anton <toshabondarev@yandex.ru>
2022-09-14 22:15:42 +0300

    lesson_4 Display model's data

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-x71u4p21yz+z36!*%t^f_uhg5z!64(c^!goa0(@^-pxazfg$xv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "markdownify.apps.MarkdownifyConfig",
     "mainapp",
 ]
 
